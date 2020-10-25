70c48816ed0aa08c07457e94c27e8732a8e56a18
malonniejackson <57572132+malonniejackson@users.noreply.github.com>
2020-10-24 22:06:09 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -24,8 +24,8 @@ function map(s1, s2){
 }
 
 document.body.addEventListener('submit', async (e) => {
-  e.preventDefault(); // this stops whatever the browser wanted to do itself.
-  const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
+  e.preventDefault(); 
+  const form = $(e.target).serializeArray(); 
   fetch('/api', {
     method: 'POST',
     headers: {
